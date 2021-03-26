7f2d0ed0a60a607e229c37ffb556a892a197a708
tienyulin <yh50114@gmail.com>
2021-03-26 13:36:18 +0000

    feat(cipher): implement ModernSymmetricCipher

--- sk_cipher.rb
+++ sk_cipher.rb
@@ -1,18 +1,28 @@
 # frozen_string_literal: true
 
+require 'rbnacl'
+
 # ModernSymmetricCipher
 module ModernSymmetricCipher
   def self.generate_new_key
     # TODO: Return a new key as a Base64 string
+
+    RbNaCl::Random.random_bytes(RbNaCl::SecretBox.key_bytes)
   end
 
   def self.encrypt(document, key)
     # TODO: Return an encrypted string
     #       Use base64 for ciphertext so that it is sendable as text
+
+    simple_box = RbNaCl::SimpleBox.from_secret_key(key)
+    simple_box.encrypt(document)
   end
 
   def self.decrypt(encrypted_cc, key)
     # TODO: Decrypt from encrypted message above
     #       Expect Base64 encrypted message and Base64 key
+
+    simple_box = RbNaCl::SimpleBox.from_secret_key(key)
+    simple_box.decrypt(encrypted_cc)
   end
 end
