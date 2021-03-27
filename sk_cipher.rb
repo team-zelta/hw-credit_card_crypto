0d601f5dd219d7ff485b3287d289563901c5fc50
tienyulin <yh50114@gmail.com>
2021-03-27 13:38:50 +0000

    feat(sk cipher): use base64 to pack message

--- sk_cipher.rb
+++ sk_cipher.rb
@@ -1,28 +1,32 @@
 # frozen_string_literal: true
 
 require 'rbnacl'
+require 'base64'
 
 # ModernSymmetricCipher
 module ModernSymmetricCipher
   def self.generate_new_key
     # TODO: Return a new key as a Base64 string
 
-    RbNaCl::Random.random_bytes(RbNaCl::SecretBox.key_bytes)
+    key = RbNaCl::Random.random_bytes(RbNaCl::SecretBox.key_bytes)
+    Base64.strict_encode64(key)
   end
 
   def self.encrypt(document, key)
     # TODO: Return an encrypted string
     #       Use base64 for ciphertext so that it is sendable as text
 
-    simple_box = RbNaCl::SimpleBox.from_secret_key(key)
-    simple_box.encrypt(document.to_s)
+    simple_box = RbNaCl::SimpleBox.from_secret_key(Base64.strict_decode64(key))
+    ciphertext = simple_box.encrypt(document.to_s)
+
+    Base64.strict_encode64(ciphertext)
   end
 
   def self.decrypt(encrypted_cc, key)
     # TODO: Decrypt from encrypted message above
     #       Expect Base64 encrypted message and Base64 key
 
-    simple_box = RbNaCl::SimpleBox.from_secret_key(key)
-    simple_box.decrypt(encrypted_cc.to_s)
+    simple_box = RbNaCl::SimpleBox.from_secret_key(Base64.strict_decode64(key))
+    simple_box.decrypt(Base64.strict_decode64(encrypted_cc).to_s)
   end
 end
